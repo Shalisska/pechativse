9ec10752b6470791b5b7bfa33bfe703eb3d49eaa
Alena <alena.simonova83@gmail.com>
2015-02-27 19:13:30 +0300

    forms

--- js/js.js
+++ js/js.js
@@ -1,16 +1,75 @@
 (function(document) {
-    var feedback = document.querySelector('.header__contacts-feedback');
-    var esc = document.querySelector('.feedback__form-esc');
-    var form = document.querySelector('.feedback__form');
+    //ищем кнопки
+    var callback = document.querySelector('.header__contacts-feedback');
+    var orders = [document.querySelector('.sidebar__ordering'), document.querySelector('.footer__ordering')];
     
-    feedback.addEventListener('click', function(event) {
-        event.preventDefault();
-        form.classList.add('form__show');
-    });
-
-    esc.addEventListener('click', function(event) {
-        event.preventDefault();
-        form.classList.remove('form__show');
-    });
-                              
+    //ищем формы
+    var callback__form = document.querySelector('.form--callback').parentNode;
+    var order__form = document.querySelector('.form--order').parentNode;
+    
+    //открытие формы обратной связи
+	callback.addEventListener('click', function(event) {
+		event.preventDefault();
+		if (callback__form.classList.contains('popup-show') != true) {
+            callback__form.classList.add('popup-show');
+        }			
+    })
+	
+    //открытие формы заказа
+	for (var i = 0; i < orders.length; i++) {
+		var order = orders[i];
+		
+		order.addEventListener('click', function(event) {
+			event.preventDefault();
+			if (order__form.classList.contains('popup-show') != true) {
+				order__form.classList.add('popup-show');
+			}			
+		})
+	}
+    
+    	//закрытие окон
+	var esc = document.querySelectorAll('.form__esc');
+	
+	for (var i = 0; i < esc.length; i++) {
+		esc[i].addEventListener('click', function(event) {
+			event.preventDefault();
+			
+			var close = this.parentNode.parentNode;
+			close.classList.remove('popup-show');
+		});       
+	}
+    
+   var shims = document.querySelectorAll('.shim');
+    
+    for(i = 0; i < shims.length; i++) {
+        var closing = function(j) {
+            var shim = shims[j];
+            
+            //закрытие по клику вне формы
+            shim.addEventListener('click', function(event) {
+                var a = event.target;
+                if(a == shim) {
+                    shim.classList.remove('popup-show');
+                }
+            });
+            
+            //закрытие по Esc
+            window.addEventListener('keydown', function(event) {
+                if (event.keyCode==27
+                   &&shim.classList.contains('popup-show')){
+                   shim.classList.remove('popup-show');
+                }
+            });
+        } (i)
+    }                       
 })(document)
+
+
+	
+	
+	
+	
+	
+
+    
+    
